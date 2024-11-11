5b11b281f4b4f3cfa982b5d94a6fecc797d68ef4
0508gyhun <0508gyhun@naver.com>
2024-11-11 14:40:49 +0900

    test

--- app/src/main/java/com/example/muapp/HomeActivity.kt
+++ app/src/main/java/com/example/muapp/HomeActivity.kt
@@ -21,7 +21,7 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge()//
 
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
